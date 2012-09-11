974e8cd771bcd2adbdd7eaea4fddd7cb3b52f01c
Aksel Forsberg <leffes.partaj@gmail.com>
2012-09-11 14:46:30 +0200

    testRoll

--- 10-pin-bowling/src/Bowling.java
+++ 10-pin-bowling/src/Bowling.java
@@ -5,25 +5,53 @@ public class Bowling {
 	public static void main(String[] args){
 	}
 	private ArrayList<Integer> rolls = new ArrayList<Integer>();
-	
+	private ArrayList<Integer> frames = new ArrayList<Integer>();
+
 	Bowling(){
 	}
-	
+
 	public void roll(int score){
-		if (score > 10 || score < 0)
+		if (score > 10 || score < 10)
 			throw new IllegalArgumentException();
 		rolls.add(score);
+		rolls.add(0);
+		frames.add(11);
 	}
-	
-	public int getRollScore(int i){
-		return rolls.get(i);
+
+	public void roll(int roll1, int roll2){
+		if (roll1 + roll2 > 10 || roll1 < 0 || roll2 < 0)
+			throw new IllegalArgumentException();
+		if (roll1 == 10)
+			roll(roll1);
+		rolls.add(roll1);
+		rolls.add(roll2);
+		if (roll1 + roll2 == 10){
+
+		}
+		frames.add(roll1+roll2);
+
+	}
+
+	public int getRollScore(int roll){
+		return rolls.get(roll);
 	}
 
 	public int getScoreSum() {
 		return 0;
 	}
 
-	public int getFrameScore() {
-		return 0;
+	public int getFrameScore(int frame) {
+		int score;
+		if (frames.get(frame-1) == 11){
+			score = 10 + rolls.get(frame*2) + rolls.get(frame*2 +1);
+			return score;
+		}
+
+		if (frames.get(frame-1) == 10){
+			score = 10 + rolls.get(frame*2);
+			return score;
+		}
+		score = frames.get(frame-1);
+		return score;
 	}
 }
--- 10-pin-bowling/src/bowlingTest.java
+++ 10-pin-bowling/src/bowlingTest.java
@@ -10,22 +10,36 @@ public class bowlingTest {
 	@Test (expected = IllegalArgumentException.class)
 	public void testRollIllegalRoll()  {
 		Bowling b = new Bowling();
+		b.roll(7, 5);
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void testStrikeRollIllegalRoll()  {
+		Bowling b = new Bowling();
 		b.roll(11);
 	}
 	
 	@Test
 	public void testRoll() {
 		Bowling b = new Bowling();
-		b.roll(5);
-		assertEquals(5, b.getRollScore(0));
+		b.roll(5, 3);
+		b.roll(3, 2);
+		b.roll(5, 5);
+		b.roll(2, 4);
+		assertEquals(8, b.getFrameScore(1));
+		assertEquals(5, b.getFrameScore(2));
+		assertEquals(12, b.getFrameScore(3));
+		assertEquals(6, b.getFrameScore(4));
 	}
 	
 	@Test
-	public void testGetFrameScore() {
+	public void testStrikeRoll() {
 		Bowling b = new Bowling();
-		assertEquals(0, b.getFrameScore());
+		b.roll(10);
+		b.roll(1, 5);
+		assertEquals(16, b.getFrameScore(1));
 	}
-
+	
 	@Test
 	public void testGetScoreSum() {
 		Bowling b = new Bowling();
